aba9beff8727140f6ab5465ee78fd1c952a8996d
wise-king-sullyman <asullivan96@live.com>
2020-09-06 14:09:07 -0400

    Refactored per ruby styling guide

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,23 +1,21 @@
+# frozen_string_literal: true
+
+# Player instances are the representations of each player
 class Player
   attr_accessor :game, :name, :symbol
-  def initialize(name,symbol)
+  def initialize(name, symbol)
     @name = name
     @symbol = symbol
   end
 
-  def get_move
-    puts "\n#{name} Enter Tile Choice"
-    gets.chomp.to_i
-  end
-
   def move
-    move_choice = self.get_move.to_i - 1
-    if @game.board.flatten[move_choice] == "_" && move_choice >= 0
-      @game.board.flatten![move_choice] = symbol
-      @game.board = unflatten(@game.board, 3)
+    puts "\n#{name} Enter Tile Choice"
+    move_choice = gets.chomp.to_i - 1
+    if valid_move?(move_choice)
+      update_board(move_choice)
     else
       puts "\n Invalid Move Choice #{name}! Try Again!"
-      self.move
+      move
     end
   end
 
@@ -27,19 +25,28 @@ class Player
 
   def unflatten(flat_array, sub_array_length)
     unflattend_array = []
-    while flat_array.size >= sub_array_length do
-      unflattend_array.push(flat_array.slice!(0...sub_array_length))
-    end
+    unflattend_array.push(flat_array.slice!(0...sub_array_length)) while \
+    flat_array.size >= sub_array_length
     unflattend_array
+  end
+
+  def valid_move?(move_choice)
+    @game.board.flatten[move_choice] == '_' && move_choice >= 0
+  end
+
+  def update_board(move_choice)
+    @game.board.flatten![move_choice] = symbol
+    @game.board = unflatten(@game.board, 3)
   end
 end
 
+# Game instances are the representation of the current match being played
 class Game
   attr_accessor :players, :board
 
   def initialize
     @game_over = false
-    @board = [['_','_','_'], ['_','_','_'], ['_','_','_']]
+    @board = [%w[_ _ _], %w[_ _ _], %w[_ _ _]]
     @players = []
   end
 
@@ -61,15 +68,14 @@ class Game
   end
 
   def game_over?(board, player)
-    if horizontal_win(board, player) || 
-      vertical_win(board, player) || 
-      diagonal_win(board, player)
-    then
+    if horizontal_win(board, player) ||
+       vertical_win(board, player) ||
+       diagonal_win(board, player)
       puts "\n #{player.name} Won!"
-      return true
+      true
     elsif tie(board)
       puts "\n Tie Game"
-      return true
+      true
     end
   end
 
@@ -83,50 +89,48 @@ class Game
   end
 
   def diagonal_win(board, player)
-    diag_1 = [board[0][0], board[1][1], board[2][2]]
-    diag_2 = [board[0][2], board[1][1], board[2][0]]
-    if all_match_symbol(diag_1, player) || all_match_symbol(diag_2, player)
-      return true
-    end
+    top_down_diagonal = [board[0][0], board[1][1], board[2][2]]
+    bottom_up_diagonal = [board[0][2], board[1][1], board[2][0]]
+    return true if all_match_symbol(top_down_diagonal, player) ||
+                   all_match_symbol(bottom_up_diagonal, player)
   end
 
   def tie(board)
-    if board.flatten.include?("_")
-      return false
-    end
+    return false if board.flatten.include?('_')
+
     true
   end
-  
+
   def all_match_symbol(array, player)
-    array.all? {|tile| tile == player.symbol}
+    array.all? { |tile| tile == player.symbol }
   end
+
   def play
     until @game_over
       @players.each do |player|
-        self.print_board
+        print_board
         player.move
-        if game_over?(@board, player)
-          self.print_board
-          @game_over = true
-          break
-        end
+        next unless game_over?(@board, player)
+
+        print_board
+        @game_over = true
+        break
       end
     end
   end
 end
 
-player_1 = Player.new('Player 1', 'X')
-player_2 = Player.new('Player 2', 'O')
+first_player = Player.new('Player 1', 'X')
+second_player = Player.new('Player 2', 'O')
 match = Game.new
 
-match.add_player(player_1)
-match.add_player(player_2)
-player_1.add_game(match)
-player_2.add_game(match)
-puts "Use the numbers 1-9 to pick a tile, ordered from top left to bottom right"
-puts "Like this: "
-p "1,2,3"
-p "4,5,6"
-p "7,8,9"
+match.add_player(first_player)
+match.add_player(second_player)
+first_player.add_game(match)
+second_player.add_game(match)
+puts 'Use the numbers 1-9 to pick a tile, ordered from top left to bottom right'
+puts 'Like this: '
+p '1,2,3'
+p '4,5,6'
+p '7,8,9'
 match.play
-
